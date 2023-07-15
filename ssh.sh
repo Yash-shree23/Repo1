e800f50a42942caa762d5763d75522c40229f3c9
Nikhil <Nikhil@gmail.com>
2023-07-15 17:12:40 +0000

    Test

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,6 @@
 #!bin/bash 
 echo Enter Your Email:
 read name 
-ssh-keygen -t rsa -b 4096 -C "$name" 
+ssh-keygen -t rsa -b 4096 -C "$name"
+ssh-add ~/.ssh/id_rsa.pub
 cat ~/.ssh/id_rsa.pub >> ssh_keygen.txt
